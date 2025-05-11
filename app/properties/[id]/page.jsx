fd89718ea1b06a96f620e63f29155211b2e08149
David Murff <dmurffkc@gmail.com>
2025-05-11 15:44:34 -0500

    Broken Tailwind Laptop

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,5 @@
-const PropertyPage = () => {
-  return <div>Property Page</div>;
+const PropertyPage = ({ params }) => {
+  return <div className="bg-blue-500">Property Page {params.id}</div>;
 };
 
 export default PropertyPage;
