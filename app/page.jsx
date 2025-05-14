c1aa2c4c6ec20290640460e1f69a80a94aee4c47
David Murff <dmurffkc@gmail.com>
2025-05-13 22:55:23 -0500

    navbar state added

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="text-3xl">Welcome</h1>
-      <h1 className="text-3xl font-bold text-blue-500">Tailwind is working?</h1>
 
       <Link href="/properties">Go To Properties</Link>
     </div>
